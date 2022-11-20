a8774339d682c769ca1cc5dff6c73a600014a9c0
bam10112002 <bam10112002@gmail.com>
2022-11-20 15:45:13 +0300

    rename newLab_3 to Lab_3, delete old Lab_3 class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 import org.example.lab1.Lab_1;
 import org.example.lab2.Lab_2;
 import org.example.lab3.Lab_3;
-import org.example.lab3.NewLab_3;
 import org.example.lab4.Lab_4;
 import org.example.lab5.Lab_5;
 import org.example.labI.Lab_Interface;
@@ -13,7 +12,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Lab_Interface[] labs = new Lab_Interface[] {new Lab_1(), new Lab_2(), new NewLab_3(), new Lab_4(), new Lab_5()};
+        Lab_Interface[] labs = new Lab_Interface[] {new Lab_1(), new Lab_2(), new Lab_3(), new Lab_4(), new Lab_5()};
 
         while(true) {
             System.out.print("input lab number: ");
--- src/main/java/org/example/lab3/Lab_3.java
+++ src/main/java/org/example/lab3/Lab_3.java
@@ -2,73 +2,80 @@ package org.example.lab3;
 
 import org.example.labI.Lab_Interface;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.util.Scanner;
-import java.util.Stack;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Lab_3 implements Lab_Interface {
+    String filename;
+    ObjectsPool pool;
+    ExecutorService executorService;
     String substring;
-    int numberOfThreads;
-    long len;
-    Stack<Long> treadsInd;   // стек стратовых точек для потоков
-    Stack<Integer> indexOf;  // стек индеков найденых подстрок
-    RandomAccessFile raf;
     @Override
-    public void Run() throws IOException {
-//        TreadPoolTest();
-        int processors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executorService = Executors.newFixedThreadPool(processors - 1);
-        try {
-            raf = new RandomAccessFile("src/main/resources/Lab_3/text.txt", "rw");
-            substring = "surprise";
-            numberOfThreads = 10;
-            treadsInd = new Stack<>();
-            len = raf.length() / numberOfThreads;
-            indexOf = new Stack<>();
-            for (int i = 0; i < numberOfThreads; i++) {
-                treadsInd.push(raf.length() / numberOfThreads * i);
-//                executorService.submit(new FinderTread().Init(substring, "src/main/resources/Lab_3/text.txt" , start, end, indexOf));
-            }
-
-            while (!treadsInd.empty()) {
-                long start = treadsInd.pop();
-                if (start >= substring.length()) { start -= substring.length(); }
-                long end = start + len;
-                if (end >= raf.length()) { end = raf.length(); }
-
-                FinderTread tread = new FinderTread();
-                tread.Init(substring, "src/main/resources/Lab_3/text.txt" , start, end, indexOf);
-                tread.start();
-//                tread.join();
-            }
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+    public void Run() {
+        File file = new File(filename);
+        int i = 0;
+        while (i < file.length()){
+            executorService.submit(new RunnableFinder().Init(i,i+1024,pool,substring));
+            i += 1024 - substring.length();
         }
-
-        int n;
-        FileWriter fw = new FileWriter("src/main/resources/Lab_3/out.txt");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input n: ");
-        n = sc.nextInt();
-
-        while (!indexOf.empty()) {
-            int curr = getPrevLen(indexOf.pop());
-            for (int i = 0; i < n ;i++) {
-                curr = getPrevLen(curr);
-            }
-            raf.seek(curr);
-            for (int i=0; i < n*2+1; i++) {
-                fw.write(raf.readLine() + "\n");
-            }
-            fw.write("\n------------------------------\n");
+        i = i - 1024 + substring.length();
+        executorService.submit(new RunnableFinder().Init(i, (int)file.length(),pool,substring));
+        executorService.shutdown();
+        try {
+            if (executorService.awaitTermination(10, TimeUnit.MINUTES))
+                System.err.println("Some process is not finished");
+            OutputStrings();
+        } catch (InterruptedException ex) {
+            System.err.println(ex.getMessage());
         }
 
     }
-    private int getPrevLen(int curr) throws IOException {
+
+    public Lab_3() throws FileNotFoundException {
+        filename = "src/main/resources/Lab_3/text.txt";
+        substring = "surprise";
+        File file = new File(filename);
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+        int processors = Runtime.getRuntime().availableProcessors();
+        executorService = Executors.newFixedThreadPool(processors - 1);
+        pool = new ObjectsPool(processors - 1, filename, file.length()/1024);
+    }
+    private void OutputStrings() {
+        if (pool.getIndexOf().isEmpty()) {
+            System.out.println("Substring not founded");
+            return;
+        }
+
+        int n;
+        try {
+            FileWriter fw = new FileWriter("src/main/resources/Lab_3/out.txt");
+            RandomAccessFile raf = new RandomAccessFile(filename, "r");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input n: ");
+            n = sc.nextInt();
+            while (!pool.getIndexOf().isEmpty()) {
+                int curr = getPrevLen(pool.getIndexOf().pop(), raf);
+                for (int i = 0; i < n ;i++) {
+                    curr = getPrevLen(curr, raf);
+                }
+                raf.seek(curr);
+                for (int i=0; i < n*2+1; i++) {
+                    fw.write(raf.readLine() + "\n");
+                }
+                fw.write("\n------------------------------\n");
+            }
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    private int getPrevLen(int curr, RandomAccessFile raf) throws IOException {
         curr -=2;
         while (curr > 0) {
             raf.seek(curr);
@@ -80,61 +87,4 @@ public class Lab_3 implements Lab_Interface {
         return -1;
     }
 
-    private void TreadPoolTest() {
-        int processors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executorService = Executors.newFixedThreadPool(processors - 1);
-
-        executorService.submit(() -> {System.out.println("Hello");});
-//        executorService.execute();
-
-        executorService.shutdown();
-    }
 }
-class FinderTread extends Thread {
-
-    RandomAccessFile raf = null;
-    String substring;
-    Long startInd;
-    Long endInd;
-    Stack<Integer> indexOf;  // стек индеков найденых подстрок
-    byte[] bytes = new byte[2048];
-
-    @Override
-    public void run() {
-        try {
-            while (startInd + 1024 < endInd) {
-                raf.seek(Math.toIntExact(startInd));
-                raf.read(bytes, 0, 1024);
-                String str = new String(bytes);
-                int ind = str.indexOf(substring);
-                if (ind != -1) {
-                    synchronized (indexOf){
-                        indexOf.push((int) (ind+startInd));
-                        System.out.println("Substring find, ind: " + ind+startInd );
-                    }
-                }
-                startInd += 1024;
-//                sleep(1); // INFO: демонстрационный функционал
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void Init(String _substring, String _path, long _start, long _end, Stack<Integer> _indexOf) {
-        try {
-            raf = new RandomAccessFile(_path, "r");
-            endInd = _end;
-            substring = _substring;
-            indexOf = _indexOf;
-            startInd = _start;
-            if (_start != 0)
-                startInd = _start - substring.length();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-}
-
